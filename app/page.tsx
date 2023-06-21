32c7cdd43faba50cb8b437bed261430cdb66b0b9
codingMustache <jorge@codingmustache.dev>
2023-06-21 10:32:17 -0500

    Title: HomePage stuff
    Details: have some basic styles and content for the HomePage
    
    Issues: need to add JSON data for the game dev group
    
    Changes to be committed:
    	new file:   app/components/city.module.css
    	new file:   app/components/city.tsx
    	new file:   app/components/footer.tsx
    	modified:   app/globals.css
    	modified:   app/layout.module.css
    	modified:   app/layout.tsx
    	new file:   app/page.module.css
    	modified:   app/page.tsx
    	new file:   public/city.png
    	new file:   public/neuropol.otf
    	new file:   public/whitrabt.ttf

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
+import City from "./components/city";
 //import the data from api
 import getData from "../api/calendar";
+import styles from './page.module.css';
 
 export default async function Home() {
   //call getData from the api to fetch
   const data = await getData();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1></h1>
-    </main>
+    <div className={styles.content}>
+      <City/>
+      <div className={styles.text}>
+        <p>> Welcome to Nola Devs, a software developers group in New Orleans! 
+          We are a  community of passionate developers dedicated to collaboration, 
+          learning, and growth. Join us to connect with like-minded professionals, 
+          expand your skills, and make a lasting impact in the world of software 
+          development. We meet in person every Tuesday at <a href="https://goo.gl/maps/HCke6uF15yJrBMvU9">The Rusty Nail</a>
+          <a className={styles.blinking}> _</a>
+        </p>
+      </div>
+    </div>
   );
 }
