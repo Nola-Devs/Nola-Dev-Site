3782f768f5c9d308a500d6b50ab5c6ee7867e104
codingMustache <jorge@codingmustache.dev>
2023-07-01 20:27:04 -0500

    Title: Maping through Json Co-authored-by: Colin <colin.williams.dev@gmail.com>

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,8 +3,15 @@ import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { Organizations } from "../types/Organizations";
 import org from "../organizations.json";
+import {
+  LinkedinOutlined,
+  MailOutlined,
+  GithubOutlined,
+  GlobalOutlined,
+  LinkOutlined,
+} from "@ant-design/icons";
 
-const organizations: Organizations = org;
+const organizations: any = org;
 
 export default function Group() {
   const location: string = usePathname().slice(1);
@@ -14,14 +21,43 @@ export default function Group() {
       <h1>{location}</h1>
       <h2>About</h2>
       <p>{organizations[location].about}</p>
-
       <h2>Organizers</h2>
       <p>
-        {organizations[location].organizers.map((e, i) => (
+        {organizations[location].organizers.map((e: any, i: number) => (
           <div key={i}>
-            <Image src={e.pfp} alt="organizer profile picture" width={100} />
+            <Image
+              src={e.pfp}
+              alt='organizer profile picture'
+              height={100}
+              width={100}
+            />
             <p>{e.name}</p>
-            {Object.keys(e.links).map((e) => e)}
+            {e.links.map((e: any) => {
+              const linked = Object.keys(e)[0];
+              let icon;
+              switch (linked) {
+                case "linkedin":
+                  icon = <LinkedinOutlined />;
+                  break;
+                case "github":
+                  icon = <GithubOutlined />;
+                  break;
+                case "email":
+                  icon = <MailOutlined />;
+                  break;
+                case "porfolio":
+                  icon = <GlobalOutlined />;
+                  break;
+                default:
+                  icon = <LinkOutlined />;
+              }
+              return (
+                <>
+                  <p>{Object.values(e)}</p>
+                  {icon}
+                </>
+              );
+            })}
           </div>
         ))}
       </p>
