08d1c66be70f87febe0227285ffbc2253dedc998
colin <colin.williams.dev@gmail.com>
2023-07-01 21:52:10 -0500

    git diff in script works
    Co-authored-by: codingmustache <contact@codingmustache.dev>

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,8 @@
 ### version 18.16.0 for consistency among team, should ensure CI builds
 export NVM_DIR=$HOME/.nvm && source $NVM_DIR/nvm.sh && nvm use v18.16.0
 
-### If node_modules is not present, then install dependencies
+### If node_modules is not present or package.json has changed, then install dependencies,
+### this is checked with git diff package.json, it checks the previous HEAD I think...
 if [ ! -d "node_modules" ] || git diff --quiet --exit-code -- package.json ; then
     npm i
 fi
