2f65bd011b7a2d715b4b94eb54a91882bf0d294a
codingMustache <jorge@codingmustache.dev>
2023-07-04 00:49:06 -0500

    🌞 🌘 mode working w switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,125 @@
 "use client";
-import "./globals.css";
+import React, { useEffect, useState } from "react";
 import { Inter } from "next/font/google";
-import styles from "./layout.module.css";
 import { organizationsStore } from "./organizations";
-import Image from "next/image";
-import { NextUIProvider, Navbar, Link } from "@nextui-org/react";
+import {
+  createTheme,
+  NextUIProvider,
+  Navbar,
+  Link,
+  Text,
+  Switch,
+  Dropdown,
+  getDocumentTheme,
+  changeTheme,
+} from "@nextui-org/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "Nola Devs",
-//   description: "The Gathering place for New Orleans' Developers",
-// };
+const lightTheme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      primaryLight: "$green200",
+      primaryLightHover: "$green300",
+      primaryLightActive: "$green400",
+      primaryLightContrast: "$green600",
+      primary: "#000",
+      primaryBorder: "$green500",
+      primaryBorderHover: "$green600",
+      primarySolidHover: "$green700",
+      primarySolidContrast: "$white",
+      primaryShadow: "$green500",
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
+      link: "#5E1DAD",
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {
+      primaryLight: "$green600",
+      primaryLightHover: "$green500",
+      primaryLightActive: "$green400",
+      primaryLightContrast: "$green100",
+      primary: "#4ADE7B",
+      primaryBorder: "$green500",
+      primaryBorderHover: "$green600",
+      primarySolidHover: "$green700",
+      primarySolidContrast: "$white",
+      primaryShadow: "$green500",
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
+      link: "#5E1DAD",
+    },
+  },
+});
+
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isDark, setIsDark] = useState(false);
+
+  const handleChange = () => {
+    const nextTheme = isDark ? "light" : "dark";
+    window.localStorage.setItem("data-theme", nextTheme); 
+    changeTheme(nextTheme);
+  };
+
+  useEffect(() => {
+    let theme = window.localStorage.getItem("data-theme");
+    setIsDark(theme === "dark");
+    const observer = new MutationObserver((mutation) => {
+      let newTheme = getDocumentTheme(document?.documentElement);
+      setIsDark(newTheme === "dark");
+    });
+    observer.observe(document?.documentElement, {
+      attributes: true,
+      attributeFilter: ["data-theme", "style"],
+    });
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <NextUIProvider>
+        <NextUIProvider theme={isDark ? darkTheme : lightTheme}>
           <Navbar isBordered variant='floating'>
-            <a href='/'>
-              <Image
-                className={styles.logo}
-                src='/logo.png'
-                alt='logo'
-                width={50}
-                height={50}
+            <Link href='/'>
+              <Text size={"20pt"} color='primary'>
+                Nola Devs
+              </Text>
+            </Link>
+            <Navbar.Content>
+              <Link href='/calendar'>Calendar</Link>
+              <Dropdown>
+                <Dropdown.Button flat>Groups</Dropdown.Button>
+                <Dropdown.Menu aria-label='Static Actions'>
+                  {Object.keys(organizationsStore).map((e, i) => (
+                    <Dropdown.Item key={i}>
+                      <Link href={`/${e}`}>{e}</Link>
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+              <Switch
+                checked={isDark}
+                size='sm'
+                onChange={handleChange}
+                iconOff={"🌞"}
+                iconOn={"🌘"}
               />
-            </a>
-            <div className={styles.links}>
-              {Object.keys(organizationsStore).map((e, i) => (
-                <Link key={i} href={`/${e}`}>
-                  {e}
-                </Link>
-              ))}
-            </div>
+            </Navbar.Content>
           </Navbar>
           {children}
         </NextUIProvider>
       </body>
-      {/* </NextUIProvider> */}
     </html>
   );
 }
