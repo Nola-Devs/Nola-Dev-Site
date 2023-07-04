85632dec16b19bbb4271be04d33d53da5b5eeb17
codingMustache <jorge@codingmustache.dev>
2023-07-03 19:15:53 -0500

    layout set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
+"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import styles from "./layout.module.css";
 import { organizationsStore } from "./organizations";
 import Image from "next/image";
-import { Navbar, Link } from 'nextui-org/react'
+import { NextUIProvider, Navbar, Link } from "@nextui-org/react";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Nola Devs",
-  description: "The Gathering place for New Orleans' Developers",
-};
+// export const metadata = {
+//   title: "Nola Devs",
+//   description: "The Gathering place for New Orleans' Developers",
+// };
 
 export default function RootLayout({
   children,
@@ -17,27 +18,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Navbar isBordered variant='floating'>
-          <a href="/">
-            <Image
-              className={styles.logo}
-              src="/logo.png"
-              alt="logo"
-              width={50}
-              height={50}
-            />
-          </a>
-          <div className={styles.links}>
-            {Object.keys(organizationsStore).map((e, i) => (
-              <Navbar.Link key={i} href={`/${e}`}>
-                {e}
-              </Navbar.Link>
-            ))}
-          </div>
-        </Navbar>
-        {children}
+        <NextUIProvider>
+          <Navbar isBordered variant='floating'>
+            <a href='/'>
+              <Image
+                className={styles.logo}
+                src='/logo.png'
+                alt='logo'
+                width={50}
+                height={50}
+              />
+            </a>
+            <div className={styles.links}>
+              {Object.keys(organizationsStore).map((e, i) => (
+                <Link key={i} href={`/${e}`}>
+                  {e}
+                </Link>
+              ))}
+            </div>
+          </Navbar>
+          {children}
+        </NextUIProvider>
       </body>
     </html>
   );
