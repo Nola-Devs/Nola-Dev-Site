8efc85b23a7c8894d86cc58aeaf46e7972cb5cc3
codingMustache <jorge@codingmustache.dev>
2023-07-06 14:29:49 -0500

    Title: Next UI Elements Details: Chanaged a tag to match next ui elements

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { Organizations, urlDTO } from "../types/index";
@@ -10,10 +9,9 @@ import {
   GlobalOutlined,
   LinkOutlined,
 } from "@ant-design/icons";
+import { Link, Card, Text } from "@nextui-org/react";
 import { organizationsStore } from "../organizations";
 import { Organizer } from "../types/Organizer";
-
-
 
 const organizations: Organizations = organizationsStore;
 
@@ -21,86 +19,96 @@ export default function Group() {
   const group: string = usePathname().slice(1);
 
   return (
-    <div >
+    <div>
       <h1>{group}</h1>
       <h2>About</h2>
       <p>{organizations[group]?.about}</p>
-      <h2>Organizers</h2>
-      {organizations[group]?.organizers.map((e: Organizer, i: number) => (
-        <div key={i}>
-          <Image
-            src={e.pfp}
-            alt='organizer profile picture'
-            height={100}
-            width={100}
-          />
-          <p>{e.name}</p>
-          <div>
-            {e.links?.map((e: urlDTO, i: number) => {
-              const linked = Object.keys(e)[0];
-              let icon;
-              switch (linked) {
-                case "linkedin":
-                  icon = (
-                    <a
-                      href={Object.values(e)[0]}
-                      target='_blank'
-                      key={Object.values(e)[0] + i}
-                    >
-                      <LinkedinOutlined />
-                    </a>
-                  );
-                  break;
-                case "github":
-                  icon = (
-                    <a
-                      href={Object.values(e)[0]}
-                      target='_blank'
-                      key={Object.values(e)[0] + i}
-                    >
-                      <GithubOutlined />
-                    </a>
-                  );
-                  break;
-                case "email":
-                  icon = (
-                    <a
-                      href={Object.values(e)[0]}
-                      target='_blank'
-                      key={Object.values(e)[0] + i}
-                    >
-                      <MailOutlined />
-                    </a>
-                  );
-                  break;
-                case "portfolio":
-                  icon = (
-                    <a
-                      href={Object.values(e)[0]}
-                      target='_blank'
-                      key={Object.values(e)[0] + i}
-                    >
-                      <GlobalOutlined />
-                    </a>
-                  );
-                  break;
-                default:
-                  icon = (
-                    <a
-                      href={Object.values(e)[0]}
-                      target='_blank'
-                      key={Object.values(e)[0] + i}
-                    >
-                      <LinkOutlined />
-                    </a>
-                  );
-                  break;
-              }
-              return icon;
-            })}
-          </div>
-        </div>
-      ))}
+      <Card
+        variant='bordered'
+        css={{ mw: "600px", margin: "50px auto", alignContent: "center" }}
+      >
+        <Card.Header>
+          <Text>
+          Organizers
+
+          </Text>
+        </Card.Header>
+        <Card.Body>
+          {organizations[group]?.organizers.map((e: Organizer, i: number) => (
+            <Text key={i}>
+              <Image
+                src={e.pfp}
+                alt='organizer profile picture'
+                height={100}
+                width={100}
+              />
+              <p>{e.name}</p>
+              {e.links?.map((e: urlDTO, i: number) => {
+                const linked = Object.keys(e)[0];
+                let icon;
+                switch (linked) {
+                  case "linkedin":
+                    icon = (
+                      <Link
+                        href={Object.values(e)[0]}
+                        target='_blank'
+                        key={Object.values(e)[0] + i}
+                      >
+                        <LinkedinOutlined />
+                      </Link>
+                    );
+                    break;
+                  case "github":
+                    icon = (
+                      <Link
+                        href={Object.values(e)[0]}
+                        target='_blank'
+                        key={Object.values(e)[0] + i}
+                      >
+                        <GithubOutlined />
+                      </Link>
+                    );
+                    break;
+                  case "email":
+                    icon = (
+                      <Link
+                        href={Object.values(e)[0]}
+                        target='_blank'
+                        key={Object.values(e)[0] + i}
+                      >
+                        <MailOutlined />
+                      </Link>
+                    );
+                    break;
+                  case "portfolio":
+                    icon = (
+                      <Link
+                        href={Object.values(e)[0]}
+                        target='_blank'
+                        key={Object.values(e)[0] + i}
+                      >
+                        <GlobalOutlined />
+                      </Link>
+                    );
+                    break;
+                  default:
+                    icon = (
+                      <Link
+                        href={Object.values(e)[0]}
+                        target='_blank'
+                        key={Object.values(e)[0] + i}
+                      >
+                        <LinkOutlined />
+                      </Link>
+                    );
+                    break;
+                }
+                return icon;
+              })}
+            </Text>
+          ))}
+        </Card.Body>
+      </Card>
     </div>
   );
 }
