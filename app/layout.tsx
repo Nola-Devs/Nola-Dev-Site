c01240c5dbd77209f688c229ff9539ebaeaf9364
loganhochwald <loganh42@gmail.com>
2023-08-03 17:11:18 -0500

    Fix Linting

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,12 @@ export default function RootLayout({
 }) {
   return (
     <html id="html" lang="en">
-      <body className={inter.className}>
+      <body className={ inter.className }>
         <NextUIProvider>
           <Navbar shouldHideOnScroll className="py-4 px-3.5">
             <NavbarBrand>
               <Link href="/">
-                <Image src="logo_light_900x844.png" alt="Logo" width={50} />
+                <Image src="logo_light_900x844.png" alt="Logo" width={ 50 } />
               </Link>
             </NavbarBrand>
             <NavbarContent justify="end">
@@ -42,8 +42,8 @@ export default function RootLayout({
                 </DropdownTrigger>
                 <DropdownMenu aria-label="Static Actions">
                   {Object.keys(organizationsStore).map((e, i) => (
-                    <DropdownItem key={i}>
-                      <Link className="w-full" href={`/${e}`}>
+                    <DropdownItem key={ i }>
+                      <Link className="w-full" href={ `/${e}` }>
                         {e.replace(/-/g, " ")}
                       </Link>
                     </DropdownItem>
