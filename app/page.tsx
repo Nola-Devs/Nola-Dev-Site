a0263f90ff77122933215e4d676ca69a5a02b778
loganhochwald <loganh42@gmail.com>
2023-08-04 15:01:16 -0500

    Change Padding on Card

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-start h-screen p-18">
-      <Card isBlurred className="mt-24 max-w-md" shadow="lg">
+    <div className="flex justify-center items-start h-screen p-4">
+      <Card isBlurred className="mt-24 max-w-md p-4" shadow="lg">
         <CardHeader className="flex justify-center">
           <h1 className="text-7xl text-center text-blue-500">NOLA Devs</h1>
         </CardHeader>
